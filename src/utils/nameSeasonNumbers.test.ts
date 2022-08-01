316424f4cf9c10ce0575d67cbbe27a4b9b7cc540
Wilrosmi <will.r.smith31@gmail.com>
2022-08-01 16:26:57 +0100

    Add nameSeasonNumbers function with test

---
+++ src/utils/nameSeasonNumbers.test.ts
@@ -0,0 +1,14 @@
+import nameSeasonNumbers from "./nameSeasonNumbers";
+
+test("Does nameSeasonNumbers return correct format for numbers over 10", () => {
+  expect(nameSeasonNumbers(16, 10)).toBe("S16E10");
+});
+
+test("Does nameSeasonNumbers return correct format for numbers over 10", () => {
+  expect(nameSeasonNumbers(8, 4)).toBe("S08E04");
+});
+
+test("Mixture of less than 10 and more than 10", () => {
+  expect(nameSeasonNumbers(8, 14)).toBe("S08E14");
+  expect(nameSeasonNumbers(26, 1)).toBe("S26E01");
+});

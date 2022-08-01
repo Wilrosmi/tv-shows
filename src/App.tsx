ed094ac4063b7313959eda5f9f13427a1a6ece6f
Wilrosmi <will.r.smith31@gmail.com>
2022-08-01 16:48:41 +0100

    Add credit to TVMaze

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import episodes from "./episodes.json";
 import EpisodeList from "./components/EpisodeList";
 
 function App(): JSX.Element {
-  return <EpisodeList episodes={episodes} />;
+  return ( 
+    <div>
+      <EpisodeList episodes={episodes} />
+      <p>Credit to <a rel="noreferrer" href="https://www.tvmaze.com/shows/82/game-of-thrones" target="_blank">TVMaze</a></p>
+    </div>
+  )
 }
 
 export default App;

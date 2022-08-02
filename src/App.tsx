bc1d3acc3531ada5d323fdba6ca2cc6e55e7b7e0
Niamh <niamh.brockbank@gmail.com>
2022-08-02 14:40:40 +0100

    Fetch episodes from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import episodes from "./episodes.json";
 import EpisodeList from "./components/EpisodeList";
 import SearchBar from "./components/SearchBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import searchFilter from "./utils/searchFilter";
+import { IEpisode } from "./utils/types";
 
 function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
+  const [episodes, setEpisodes] = useState<IEpisode[]>([])
+
+  useEffect(() => {
+    const fetchEpisodes = async () => {
+      const response = await fetch(
+        "https://api.tvmaze.com/shows/82/episodes"
+      );
+      const jsonBody: IEpisode[] = await response.json();
+      setEpisodes(jsonBody)
+    }
+    fetchEpisodes();
+  }, []);
   const filteredEpList = searchFilter(searchTerm, episodes);
 
   return (
